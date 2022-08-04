2c78510bad3dec435883006bf8228f5bbbd28b63
codingcorgi <tbvjsks21@naver.com>
2022-08-04 11:49:24 +0900

    test breagrow article 2

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -3,20 +3,20 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
-<<<<<<< HEAD
+# <<<<<<< HEAD
 from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView, ArticleListView
-=======
+# =======
 from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
->>>>>>> origin/main
+# >>>>>>> origin/main
 
 app_name ='accountapp'
 
 urlpatterns =[
-<<<<<<< HEAD
+# <<<<<<< HEAD
     path('list/', ArticleListView.as_view(), name='list'),
-=======
+# =======
     path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
->>>>>>> origin/main
+# >>>>>>> origin/main
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
     path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
