7dba14772c8f3ccc86f077dd226604f08b8805b1
codingcorgi <tbvjsks21@naver.com>
2022-08-10 18:16:14 +0900

    docker secret

---
+++ breadgrow/settings/deploy.py
@@ -0,0 +1,59 @@
+from .base import *
+
+def read_secret(secret_name):
+    file =open('/run/secrets/' +secret_name)
+    secret =file.read()
+    secret =secret.rstrip().lstrip()
+    file.close()
+
+    return secret
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# environ.Env.read_env(os.path.join(BASE_DIR, '../../.env'))
+
+
+
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# CSRF_TRUSTED_ORIGINS = ['breadgrow.me']
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': read_secret('MYSQL_PASSWORD'),
+        'HOST': 'mariadb',
+        'PORT': '3306',
+    }
+}
