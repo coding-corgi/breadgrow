ba2d0c10e3a10918d06cc503c5ef5fdc04ebd1d2
codingcorgi <tbvjsks21@naver.com>
2022-08-11 19:56:37 +0900

    local.py

--- breadgrow/settings/local.py
+++ breadgrow/settings/local.py
@@ -30,7 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/']
+# CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/']
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
