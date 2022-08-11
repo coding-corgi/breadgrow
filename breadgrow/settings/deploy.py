1dbca0069b8b1f87d76e5f26084b63a749d922a9
codingcorgi <tbvjsks21@naver.com>
2022-08-11 12:05:29 +0900

    deploy fix1

--- breadgrow/settings/deploy.py
+++ breadgrow/settings/deploy.py
@@ -52,7 +52,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': read_secret('MYSQL_PASSWORD'),
+        'PASSWORD': 'password1234',
         'HOST': 'mariadb',
         'PORT': '3306',
     }
